6004e8fef11565be120ea442c1850d51bbef4754
tomiyuichi <tomiyuichi@inspiron5491_pre_petr_wsl.com>
2024-11-18 12:47:33 +0900

    added vite and home/login/register OK. but welcome(landing) page is bugy.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return view('home');
 });
+
+Route::get('/logout', function () {
+    Auth::logout();
+    return redirect('/login'); // ログアウト後のリダイレクト先を指定
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
