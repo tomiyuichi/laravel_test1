7d9922f36ae55e0764b7d1df8431185368dbf563
tomiyuichi <tomiyuichi@inspiron5491_pre_petr_wsl.com>
2024-11-20 17:23:12 +0900

    now pagonation works well and total number of items filtered is shown on page.

--- app/Http/Controllers/Auto_mpg_Controller.php
+++ app/Http/Controllers/Auto_mpg_Controller.php
@@ -9,6 +9,28 @@ class Auto_mpg_Controller extends Controller
 {
     public function index(Request $request)
     {
+
+        $limits = [
+            'debug_1' => 'hoge',
+            'debug_2' => 'hige',
+            'displacement' => [
+                'min' => Auto_mpg::min('displacement'),
+                'max' => Auto_mpg::max('displacement'),
+            ],
+            'mpg' => [
+                'min' => Auto_mpg::min('mpg'),
+                'max' => Auto_mpg::max('mpg'),
+            ],
+            'cylinders' => [
+                'min' => Auto_mpg::min('cylinders'),
+                'max' => Auto_mpg::max('cylinders'),
+            ],
+            'horsepower' => [
+                'min' => Auto_mpg::min('horsepower'),
+                'max' => Auto_mpg::max('horsepower'),
+            ],
+        ];
+
         // 絞り込み条件を取得
         $query = Auto_mpg::query();
 
@@ -20,14 +42,26 @@ class Auto_mpg_Controller extends Controller
             $query->where('cylinders', $request->cylinders);
         }
 
-        if ($request->filled('min_horsepower') && $request->filled('max_horsepower')) {
-            $query->whereBetween('horsepower', [$request->min_horsepower, $request->max_horsepower]);
+        // if ($request->filled('min_horsepower') && $request->filled('max_horsepower')) {
+        //     $query->whereBetween('horsepower', [$request->min_horsepower, $request->max_horsepower]);
+        // }
+
+        // min以上
+        if ($request->filled('min_horsepower')){
+            $query->where('horsepower', '>=', $request->min_horsepower);
+        }
+        // max以下
+        if ($request->filled('max_horsepower')){
+            $query->where('horsepower', '<=', $request->max_horsepower);
         }
 
         // 絞り込んだ結果を取得
-        // var_dump($query);
-        $cars = $query->paginate(10);
+        // dump($request -> session() -> get('cylinders'));
+        dump($request -> query() );
+        $cars = $query
+            ->paginate(10)                // 1ページあたり10件
+            ->appends($request->query()); // クエリパラメータを保持
 
-        return view('auto_mpg.index', compact('cars'));
+        return view('auto_mpg.index', compact('cars', 'limits'));
     }
 }
--- resources/views/auto_mpg/index.blade.php
+++ resources/views/auto_mpg/index.blade.php
@@ -11,50 +11,60 @@
         <div class="form-row">
             <div class="form-group col-md-3">
                 <label for="mpg">MPG</label>
-                <input type="number" class="form-control" id="mpg" name="mpg" value="{{ request('mpg') }}">
+                <input type="number" class="form-control" id="mpg" name="mpg" min={{$limits["mpg"]["min"]}} max={{$limits["mpg"]["max"]}} placeholder="mpgを入力" value="{{ request('mpg') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="cylinders">Cylinders</label>
-                <input type="number" class="form-control" id="cylinders" name="cylinders" value="{{ request('cylinders') }}">
+                <input type="number" class="form-control" id="cylinders" name="cylinders" min={{$limits["cylinders"]["min"]}} max={{$limits["cylinders"]["max"]}} placeholder="cylindersを入力" value="{{ request('cylinders') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="min_horsepower">MIN Horsepower</label>
-                <input type="number" class="form-control" id="min_horsepower" name="min_horsepower" value="{{ request('min_horsepower') }}">
+                <input type="number" class="form-control" id="min_horsepower" name="min_horsepower" min={{$limits["horsepower"]["min"]}} max={{$limits["horsepower"]["max"]}} placeholder="horsepowerを入力" value="{{ request('min_horsepower') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="max_horsepower">MAX Horsepower</label>
-                <input type="number" class="form-control" id="max_horsepower" name="max_horsepower" value="{{ request('max_horsepower') }}">
+                <input type="number" class="form-control" id="max_horsepower" name="max_horsepower" min={{$limits["horsepower"]["min"]}} max={{$limits["horsepower"]["max"]}} placeholder="horsepowerを入力" value="{{ request('max_horsepower') }}">
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Filter</button>
     </form>
 
     <!-- 絞り込み結果表示 -->
-    <table class="table mt-4">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Car Name</th>
-                <th>MPG</th>
-                <th>Cylinders</th>
-                <th>Horsepower</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- $cars comes from Auto_mpg_Controller -->
-            @foreach ($cars as $car)
-                <tr>
-                    <td>{{ $car->id }}</td>
-                    <td>{{ $car->car_name }}</td>
-                    <td>{{ $car->mpg }}</td>
-                    <td>{{ $car->cylinders }}</td>
-                    <td>{{ $car->horsepower }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @if(!$cars->isEmpty())
+        <div class="alert alert-info mt-4">
+            絞り込み条件に合致するレコード数: <strong>{{ $cars->total() }}</strong>
+        </div>
+    @endif
 
-    <!-- ページネーション -->
-    {{ $cars->links() }}
+    @if($cars->isEmpty())
+        <div class="alert alert-warning">結果が見つかりませんでした。</div>
+    @else
+        <table class="table mt-4">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Car Name</th>
+                    <th>MPG</th>
+                    <th>Cylinders</th>
+                    <th>Horsepower</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- $cars comes from Auto_mpg_Controller -->
+                @foreach ($cars as $car)
+                    <tr>
+                        <td>{{ $car->id }}</td>
+                        <td>{{ $car->car_name }}</td>
+                        <td>{{ $car->mpg }}</td>
+                        <td>{{ $car->cylinders }}</td>
+                        <td>{{ $car->horsepower }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <!-- ページネーション -->
+        {{ $cars->links() }}
+    @endif
 </div>
 @endsection
